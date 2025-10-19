ece1e057bd6968c0d40f5eadcdef9f4acf3123d1
kwm42 <kwm2596123568@gmail.com>
2025-10-19 14:55:15 +0800

    mdx 配置

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,23 @@
 import type { NextConfig } from "next";
+import createMDX from '@next/mdx'
 import createNextIntlPlugin from "next-intl/plugin";
 import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+}
 
-export default withNextIntl(nextConfig);
+const withMDX = createMDX({
+  extension: /\.(md|mdx)$/,
+  // Add markdown plugins here, as desired
+})
+
+export default withMDX(withNextIntl(nextConfig));
 
 // added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
 initOpenNextCloudflareForDev();
---
+++ src/app/[locale]/mdx/page.tsx
@@ -0,0 +1,5 @@
+import Welcome from '@/content/what.mdx'
+ 
+export default function Page() {
+  return <Welcome />
+}
